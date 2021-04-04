bec30cc3c2bf49b22d73628b513b1c32c9c22c23
masaki obayashi <tusanch6@gmail.com>
2021-04-04 16:56:07 +0900

    add hack mode logic

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,11 @@ use std::{thread, time};
 use std::fs::File;
 use std::io::prelude::*;
 
+// logging用の時間に関するパッケージ
+use chrono::{Utc, DateTime};
+
+use rand::Rng;
+
 pub struct Installer{
     pub mode: Mode
 }
@@ -19,11 +24,9 @@ impl Installer{
     pub fn run(&self){
         match &self.mode{
             Mode::InstallFile => {
-                // 
                 &self.run_installfile_mode();
             }
             Mode::Hacking => {
-                println!("hacking mode");
                 &self.run_hacking_mode();
             }
         }
@@ -41,13 +44,19 @@ impl Installer{
 
     // 具体的なハッキングモードの処理
     fn run_hacking_mode(&self){
+        let filenames = get_file_strings(&self.mode);
+        let vec_filename: Vec<&str> = filenames.split_whitespace().collect();
 
+        for filename in vec_filename{
+            fake_log(filename);
+        }
+        
     }
 }
 
 // 偽のシーケンスバーを表示
 pub fn fake_progressbar(filename : &str){
-    let ten_millis = time::Duration::from_millis(100);
+    let mut rng = rand::thread_rng();
     let total_size = 100;
     
     let bar = ProgressBar::new(total_size);
@@ -57,11 +66,28 @@ pub fn fake_progressbar(filename : &str){
     bar.set_style(sty);
     
     for _ in 0..total_size {
-        bar.set_message(&format!("dounload {}", filename));
+        // リアル感のためのスリープ
+        let sleep_time = rng.gen_range(10..50);
+        bar.set_message(&format!("download {}", filename));
         bar.inc(1);
-        thread::sleep(ten_millis);
+        thread::sleep(time::Duration::from_millis(sleep_time));
     }
     bar.finish_with_message("downloaded");
+}
+
+pub fn fake_log(file_name: &str){
+    let mut rng = rand::thread_rng();
+    let sleep_time = rng.gen_range(10..50);
+    logger(file_name, "start");
+    thread::sleep(time::Duration::from_millis(sleep_time));
+    fake_progressbar(file_name);
+    logger(file_name, "end");
+}
+
+// logger
+pub fn logger(process_name : &str, message: &str){
+    let utc_datetime: DateTime<Utc> = Utc::now();
+    println!("[{}   :{}] {}", utc_datetime, process_name, message);
 }
 
 // file名などに使用する辞書 txtフォルダ
