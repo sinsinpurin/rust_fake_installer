846793b5ce207307ffc02b485f72337d32c0c2be
masaki obayashi <tusanch6@gmail.com>
2021-04-04 13:18:48 +0900

    fix mode

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,35 @@
 extern crate fake_installer;
 
+use std::env;
 use std::{process};
 
 use fake_installer::{Installer, Mode};
 fn main() {
-    let f_installer = Installer::new(3, Mode::InstallerFile).unwrap_or_else(|err|{
-            eprintln!("Problem!! : {}", err);
-            process::exit(1);
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 2 {
+        let mode;
+        match args[1].as_str(){
+            "installfile" => {
+                mode = Mode::InstallFile;
+            }
+            "hacking" => {
+                mode = Mode::Hacking;
+            }
+            _ => {
+                eprintln!("Error: Please input collect mode name.");
+                process::exit(10);
+            }
         }
-    );
-    f_installer.run();
+
+        let f_installer = Installer::new(mode).unwrap_or_else(|err|{
+                eprintln!("Problem!! : {}", err);
+                process::exit(1);
+            }
+        );
+        f_installer.run();
+    }else{
+        eprintln!("Error: Args isn't enough.");
+        process::exit(10);
+    }
     println!("Hello, world!");
 }
